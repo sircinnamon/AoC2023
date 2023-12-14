dcd2c72329caa69c49900e2ed63e4ab8359c6729
Riley Lahd <sircinnamon@gmail.com>
2023-12-14 12:58:45 -0700

    Day 2 complete

---
+++ 02/pt1.py
@@ -0,0 +1,30 @@
+import re
+filename = "input.txt"
+# filename = "example.txt"
+
+with open(filename, "r") as f:
+	line = f.readline().strip()
+	games = []
+	while line:
+		maxes = [0,0,0] # RGB
+		for rnd in line.split(":")[1].split(";"):
+			# print("\t"+rnd)
+			if "red" in rnd:
+				r = int(re.search("(\\d+) red", rnd).group(1))
+				if r > maxes[0]: maxes[0]=r
+			if "green" in rnd:
+				g = int(re.search("(\\d+) green", rnd).group(1))
+				if g > maxes[1]: maxes[1]=g
+			if "blue" in rnd:
+				b = int(re.search("(\\d+) blue", rnd).group(1))
+				if b > maxes[2]: maxes[2]=b
+			# print(maxes)
+		games.append(maxes)
+
+		line = f.readline().strip()
+	total=0
+	for i,g in enumerate(games):
+		if(g[0] <= 12 and g[1] <= 13 and g[2] <= 14):
+			total+=i+1
+	# print(games)
+	print(total)
