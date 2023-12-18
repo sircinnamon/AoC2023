25dad83429d05d3f713284f283fd508eef9fbd71
Riley Lahd <sircinnamon@gmail.com>
2023-12-18 13:36:16 -0700

    Day 9 complete
    Got stuck on part 1 because i was summing the arr to check if it was all 0s. Silly mistake.
    
    Part 2 added 5 characters...

---
+++ 09/pt2.py
@@ -0,0 +1,21 @@
+filename = "input.txt"
+# filename = "example.txt"
+
+def predict_next(seq):
+	if(len(set(seq)) == 1): return seq[0]
+	deriv = []
+	for i,x in enumerate(seq[1:]):
+		# print(x, seq[i])
+		deriv.append(x-seq[i])
+	nxt = predict_next(deriv)+seq[-1]
+	# print(seq, nxt)
+	return nxt
+
+total = 0
+with open(filename, "r") as f:
+	line = f.readline().strip()
+	while line:
+		seq = [int(x) for x in line.split(" ")]
+		total+=predict_next(seq[::-1])
+		line = f.readline().strip()
+print(total)
